1bdfccb82b47be1c509fcece2134f01ec2cb302c
Deep Shah <dshah0405@gmail.com>
2021-05-09 11:28:52 +0530

    Update settings.py

--- alphacrew/alphacrew/alphacrew/settings.py
+++ alphacrew/alphacrew/alphacrew/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-frqyg%(olbcj%h@(n51b-i6^_^2+cjo_u+b=y7m%19ck5g*lh*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
